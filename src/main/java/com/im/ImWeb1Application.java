cf52770f870661f017bb69f954eab5e6b0ada1d8
Yang ZhongChao <yangzhongchao@credittag.io>
2018-04-11 11:05:04 +0800

    123

---
+++ src/main/java/com/im/ImWeb1Application.java
@@ -0,0 +1,12 @@
+package com.im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImWeb1Application.class, args);
+	}
+}
